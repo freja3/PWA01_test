7843678c3a6750ffd7735e91891997eb704401fc
freja3 <52329311+freja3@users.noreply.github.com>
2026-04-15 11:35:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('fetch', function(event) {
-    // Dette holder appen kørende (tom for nu)
+const CACHE_NAME = 'temp-app-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Installer og cache filer
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Netværksstrategi: Prøv netværk, ellers cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
